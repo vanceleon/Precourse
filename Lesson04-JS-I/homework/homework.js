d034101d93db3f5c0b4108bcc59433b13fa3a06b
vanceleon44@gmail.com <vanceleon>
2018-05-03 16:15:57 -0400

    Lesson04 HW

--- Lesson04-JS-I/homework/homework.js
+++ Lesson04-JS-I/homework/homework.js
@@ -1,152 +1,252 @@
 //In these first 6 questions, replace `null` with the answer
 
 //create a string variable, it can contain anything
-const newString = null ;
+const newString = "Vance Leon" ;
 
 //create a number variable, it an be any number
-const newNum = null ;
+const newNum = 44 ;
 
 //create a boolean variable
-const newBool = null ;
+const newBool = true ;
 
 //solve the following math problem
-const newSubtract = 10 - null === 5;
+const newSubtract = 10 - 5 === 5;
 
 //Solve the following math problem
-const newMultiply = 10 * null === 40 ;
+const newMultiply = 10 * 4 === 40 ;
 
 //Solve the following math problem:
-const newModulo = 21 % 5 === null ;
+const newModulo = 21 % 5 === 1 ;
 
 
 
-//In the next 22 problems you will compete the function. All of your code will go inside of the function braces. 
+//In the next 22 problems you will compete the function.
+//All of your code will go inside of the function braces.
 //Make sure you use return when the prompt asks you to.
-//hint: console.log() will NOT work. 
+//hint: console.log() will NOT work.
 //Do not change any of the function names
 
-function returnString(str) {
+const str12 = function returnString(str) {
   //simply return the string provided: str
+  str = "Vance";
+  console.log(str);
+
 }
 
-function add(x, y) {
-  // x and y are numbers
-  // add x and y together and return the value
-  // code here
-}
+str12();
 
-function subtract(x, y) {
+const add = function add(x, y) {
+
+  x = 5;
+  y = 6;
+  return x + y;
+
+};
+add();
+
+const sub = function subtract(x, y) {
   // subtract y from x and return the value
   // code here
-}
+  x = 5
+  y = 5
+  return x - y;
+};
+sub();
 
-function multiply(x, y) {
+const mult = function multiply(x, y) {
   // multiply x by y and return the value
   // code here
-}
+  x = 9
+  y = 9
+  return x * y;
+};
+mult();
 
-function divide(x, y) {
-  // divide x by y and return the value
+const num = function divide(x, y) {
+  // multiply x by y and return the value
   // code here
-}
+  x = 96
+  y = 12
+  return x /= y;
+};
+num();
 
-function areEqual(x, y) {
-  // return true if x and y are the same
-  // otherwise return false
+const num = function areEqual(x, y) {
+  // multiply x by y and return the value
   // code here
-}
+  x = 12
+  y = 12
+  return x == y;
+};
+num();
 
-function areSameLength(str1, str2) {
+const sameLength = function areSameLength(str1, str2) {
   // return true if the two strings have the same length
   // otherwise return false
   // code here
-}
+  str1 = "Vance";
+  str2 = "Leon";
 
-function lessThanNinety(num) {
-  // return true if the function argument: num , is less than ninety
+  if(console.log(str1.length) !== console.log(str2.length)) {
+      return false;
+
+  }
+      return true;
+}
+sameLength();
+
+const greatThan = function lessThanNinety(num) {
+  // return true if the two strings have the same length
   // otherwise return false
   // code here
-}
 
-function greaterThanFifty(num) {
+  if(num < 90) {
+      return true;
+
+  }
+      return false;
+};
+greatThan(100);
+
+const greatThan1 = function greaterThanFifty(num) {
   // return true if num is greater than fifty
   // otherwise return false
   // code here
-}
 
-function getRemainder(x, y) {
+if(num > 50) {
+    return true;
+
+}
+    return false;
+};
+greatThan1(100);
+
+const remain = function getRemainder(x, y) {
   // return the remainder from dividing x by y
   // code here
-}
+  x = 51;
+  y = 5;
+  return x % y
 
-function isEven(num) {
+}
+remain();
+
+
+const evenNum = function isEven(num) {
   // return true if num is even
   // otherwise return false
   // code here
-}
 
-function isOdd(num) {
-  // return true if num is odd
+  if (num % 2 == 0) {
+      return true;
+  }
+      return false;
+}
+evenNum(24);
+
+const oddNum = function isEven(num) {
+  // return true if num is even
   // otherwise return false
   // code here
-}
 
-function square(num) {
+  if (num % 2 == 1) {
+      return true;
+  }
+      return false;
+}
+oddNum(25);
+
+
+const expon = function square(num) {
   // square num and return the new value
   // hint: NOT square root!
+  return Math.pow(4,2);
   // code here
 }
+expon();
 
-function cube(num) {
-  // cube num and return the new value
-  // code here
+
+const expon2 = function cube(num) {
+
+  return Math.pow(4,3);
 }
+expon2();
 
-function raiseToPower(num, exponent) {
-  // raise num to whatever power is passed in as exponent
-  // code here
+
+
+const expon3 = function raiseToPower(num, exponent) {
+
+  return Math.pow(4,20);
 }
+expon3();
 
-function roundNumber(num) {
-  // round num and return it
-  // code here
+
+const numRound = function roundNumber(num) {
+
+  return Math.round(7.98);
 }
+numRound();
 
-function roundUp(num) {
-  // round num up and return it
-  // code here
+
+const numRound2 = function roundUp(num) {
+
+  return Math.ceil(7.2);
 }
+numRound2();
 
-function addExclamationPoint(str) {
+
+const exclamationPoint = function addExclamationPoint(str) {
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
-}
+   console.log(`${statement}`,'!');
 
-function combineNames(firstName, lastName) {
+}
+const statement = 'Hello';
+exclamationPoint(statement);
+
+const fullName = function combineNames(firstName, lastName) {
   // return firstName and lastName combined as one string and separated by a space.
   // 'Lambda', 'School' -> 'Lambda School'
   // code here
+  firstName = "Vance";
+  lastName = "Leon";
+  fullName = firstName + lastName;
+  return fullName;
 }
+fullName();
 
-function getGreeting(name) {
+
+const hiStat = function getGreeting(name) {
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
+  console.log(`${statement}`, " Hello, I am Vance");
 }
+const statement = "Dan"
+hiStat(statement);
 
-// The next three questions will have you implement math area formulas. 
+// The next three questions will have you implement math area formulas.
 // If you can't remember these area formulas then head over to Google.
- 
-function getRectangleArea(length, width) {
+
+const areaRect = function getRectangleArea(length, width) {
   // return the area of the rectangle by using length and width
   // code here
+  length = 9
+  width = 12
+  return length * width;
 }
+areaRect();
 
-function getTriangleArea(base, height) {
+const areaTri = function getTriangleArea(base, height) {
   // return the area of the triangle by using base and height
   // code here
+  length = 9
+  width = 12
+  return (length * width)/2;
+
 }
+areaTri();
 
 // Do not modify code below this line.
 // --------------------------------
